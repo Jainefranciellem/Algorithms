6bf7d67500ed107426f5d4bdc6c69ab212b6c547
Jainefranciellem <jainefraciellen@gmail.com>
2023-07-06 18:26:04 -0300

    fix errors

--- tests/test_encrypt_message.py
+++ tests/test_encrypt_message.py
@@ -2,36 +2,13 @@ import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
 
-def test_encrypt_message_reversed():
-    message = "Hello, world!"
-    key = 15
-    expected = "!dlrow ,olleH"
-    assert encrypt_message(message, key) == expected
+def test_encrypt_message():
+    assert encrypt_message("Hello, world!", 15) == "!dlrow ,olleH"
 
+    assert encrypt_message("Hello, world!", 6) == "!dlrow _,olleH"
 
-def test_encrypt_message_two_parts():
-    message = "Hello, world!"
-    key = 6
-    expected = "!dlrow _,olleH"
-    assert encrypt_message(message, key) == expected
-
-
-def test_encrypt_message_odd_key():
-    message = "Hello, world!"
-    key = 7
-    expected = " ,olleH_!dlrow"
-    assert encrypt_message(message, key) == expected
-
-
-def test_encrypt_message_invalid_key_type():
-    message = "Hello, world!"
-    key = "invalid"
     with pytest.raises(TypeError):
-        encrypt_message(message, key)
+        encrypt_message("Hello, world!", "invalid")
 
-
-def test_encrypt_message_invalid_message_type():
-    message = 12345
-    key = 5
     with pytest.raises(TypeError):
-        encrypt_message(message, key)
+        encrypt_message(12345, 5)
