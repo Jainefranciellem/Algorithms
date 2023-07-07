fcd3256a482e675f4a8f079f6ad794d890a47af1
Jainefranciellem <jainefraciellen@gmail.com>
2023-07-07 09:55:08 -0300

    fix errors 3

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("Hello, world!", 15) == "!dlrow ,olleH"
+
+    assert encrypt_message("Hello, world!", 6) == "!dlrow _,olleH"
+
+    assert encrypt_message("Hello, world!", 0) == "!dlrow ,olleH"
+
+    with pytest.raises(TypeError):
+        encrypt_message("Hello, world!", "invalid")
+
+    with pytest.raises(TypeError):
+        encrypt_message(12345, 5)
