b6f123abe7b360babc80b89fe87dd7a7ad799b27
Jainefranciellem <jainefraciellen@gmail.com>
2023-07-07 17:06:55 -0300

    feat: challenge duplicate done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if not isinstance(nums[i], int) or nums[i] <= 0:
+            print(nums[i])
+            return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False
+
+
+find_duplicate([1, 2, 2, 3, 4])
